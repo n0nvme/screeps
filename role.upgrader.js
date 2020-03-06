61aa343c2b421d721e13f2a7517423965866c49a
n0nvme <svmpl3nvm3@gmail.com>
2020-03-06 14:43:38 +0300

    reformat using VSCode

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,21 +3,20 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
             creep.say('🔄 refil');
         }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
             creep.memory.upgrading = true;
             creep.say('⚡ upgrade');
         }
 
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
-        }
-        else {
+        } else {
             //var sources = creep.room.find(FIND_SOURCES);
             //if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
             //    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
@@ -28,9 +27,9 @@ var roleUpgrader = {
                         structure.structureType == STRUCTURE_SPAWN) && structure.energy <= structure.energyCapacity;
                 }
             });
-            if(targets.length > 0) {
-                if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            if (targets.length > 0) {
+                if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
                 }
             }
         }
