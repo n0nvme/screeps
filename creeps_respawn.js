d85a744a38b7798270c0419b75a17728030e486f
n0nvme <svmpl3nvm3@gmail.com>
2020-03-07 04:10:56 +0300

    some refactor & add advanced spawn logic

--- creeps_respawn.js
+++ creeps_respawn.js
@@ -3,30 +3,47 @@ var creeps_spawn = {
     run: function(game) {
         var harvesters1 = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.memory.target == 1);
         var harvesters0 = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.memory.target == 0);
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+
         // if(harvesters1.length < 2) {
         //     var newName = 'HarvesterBIG' + Game.time;
         //     console.log('Spawning new big harvester: ' + newName);
         //     Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, 
         //         {memory: {role: 'harvester', target: 1}});
         // }
-        if (harvesters1.length < 4) {
+        if (harvesters1.length < 2 && game.rooms[0].energyAvailable < 550 && game.rooms[0].energyAvailable >= 300) {
             var newName = 'Harvester' + Game.time;
             console.log('Spawning new harvester: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', target: 1 } });
+        } else if (harvesters1.length < 2 && game.rooms[0].energyAvailable >= 550) {
+            var newName = 'HarvesterBIG' + Game.time;
+            console.log('Spawning new big harvester: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester', target: 1 } });
         }
-        // if(harvesters0.length < 2) {
-        //     var newName = 'HarvesterBIG' + Game.time;
-        //     console.log('Spawning new big harvester: ' + newName);
-        //     Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], newName, 
-        //         {memory: {role: 'harvester', target: 0}});
-        // }
-        if (harvesters0.length < 3) {
+
+        if (harvesters0.length < 2 && game.rooms[0].energyAvailable < 550) {
             var newName = 'Harvester' + Game.time;
             console.log('Spawning new harvester: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
+        } else if (harvesters0.length < 2 && game.rooms[0].energyAvailable >= 550) {
+            var newName = 'HarvesterBIG' + Game.time;
+            console.log('Spawning new big harvester: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
         }
 
-        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+        // if (harvesters0.length < 2) {
+        //     var newName = 'HarvesterBIG' + Game.time;
+        //     console.log('Spawning new big harvester: ' + newName);
+        //     Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
+        // }
+        // if (harvesters0.length < 3) {
+        //     var newName = 'Harvester' + Game.time;
+        //     console.log('Spawning new harvester: ' + newName);
+        //     Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
+        // }
+
 
         // if(builders.length < 2) {
         //     var newName = 'BuilderBIG' + Game.time;
@@ -39,7 +56,6 @@ var creeps_spawn = {
             console.log('Spawning new builder: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'builder' } });
         }
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
 
         // if(upgraders.length < 2) {
         //     var newName = 'UpgraderBIG' + Game.time;
