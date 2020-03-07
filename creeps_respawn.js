2fcfbbd9822df2f1124b2d8777c485fce45fb85a
n0nvme <svmpl3nvm3@gmail.com>
2020-03-07 05:37:04 +0300

    fix spawn

--- creeps_respawn.js
+++ creeps_respawn.js
@@ -5,56 +5,41 @@ var creeps_spawn = {
         var harvesters0 = _.filter(game.creeps, (creep) => creep.memory.role == 'harvester' && creep.memory.target == 0);
         var builders = _.filter(game.creeps, (creep) => creep.memory.role == 'builder');
         var upgraders = _.filter(game.creeps, (creep) => creep.memory.role == 'upgrader');
-
-        // if(harvesters1.length < 2) {
-        //     var newName = 'HarvesterBIG' + game.time;
-        //     console.log('Spawning new big harvester: ' + newName);
-        //     game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, 
-        //         {memory: {role: 'harvester', target: 1}});
-        // }
-        if (harvesters1.length < 2 && game.rooms['W12S3'].energyAvailable < 550 && game.rooms['W12S3'].energyAvailable >= 250) {
+        
+        if (harvesters1.length < 2 && game.rooms['W12S3'].energyAvailable < 550 && game.rooms['W12S3'].energyAvailable >= 300) {
             var newName = 'Harvester' + game.time;
             console.log('Spawning new harvester: ' + newName);
             game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', target: 1 } });
+            return
         } else if (harvesters1.length < 2 && game.rooms['W12S3'].energyAvailable >= 550) {
             var newName = 'HarvesterBIG' + game.time;
             console.log('Spawning new big harvester: ' + newName);
             game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester', target: 1 } });
+            return
         }
 
-        if (harvesters0.length < 2 && game.rooms['W12S3'].energyAvailable < 550 && game.rooms['W12S3'].energyAvailable >= 250) {
+        if (harvesters0.length < 2 && game.rooms['W12S3'].energyAvailable < 550 && game.rooms['W12S3'].energyAvailable >= 300) {
             var newName = 'Harvester' + game.time;
             console.log('Spawning new harvester: ' + newName);
             game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
+            return
         } else if (harvesters0.length < 2 && game.rooms['W12S3'].energyAvailable >= 550) {
             var newName = 'HarvesterBIG' + game.time;
             console.log('Spawning new big harvester: ' + newName);
             game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
+            return
         }
-
-
-        // if (harvesters0.length < 2) {
-        //     var newName = 'HarvesterBIG' + game.time;
-        //     console.log('Spawning new big harvester: ' + newName);
-        //     game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
-        // }
-        // if (harvesters0.length < 3) {
-        //     var newName = 'Harvester' + game.time;
-        //     console.log('Spawning new harvester: ' + newName);
-        //     game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', target: 0 } });
-        // }
-
-
-        // if(builders.length < 2) {
-        //     var newName = 'BuilderBIG' + game.time;
-        //     console.log('Spawning new big builder: ' + newName);
-        //     game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], newName, 
-        //         {memory: {role: 'builder'}});
-        // }
-        if (builders.length < 2, game.rooms['W12S3'].energyAvailable >= 250) {
+        if (upgraders.length < 3, game.rooms['W12S3'].energyAvailable >= 300) {
+            var newName = 'Upgrader' + game.time;
+            console.log('Spawning new upgrader: ' + newName);
+            game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'upgrader' } });
+            return
+        }
+        if (builders.length < 2, game.rooms['W12S3'].energyAvailable >= 300) {
             var newName = 'Builder' + game.time;
             console.log('Spawning new builder: ' + newName);
             game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'builder' } });
+            return
         }
 
         // if(upgraders.length < 2) {
@@ -63,11 +48,7 @@ var creeps_spawn = {
         //     game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE], newName, 
         //         {memory: {role: 'upgrader'}});
         // }
-        if (upgraders.length < 3, game.rooms['W12S3'].energyAvailable >= 250) {
-            var newName = 'Upgrader' + game.time;
-            console.log('Spawning new upgrader: ' + newName);
-            game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'upgrader' } });
-        }
+        
 
     }
 };
