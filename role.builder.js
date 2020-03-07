5eb70e15a547ce229676a78506a4c025545d5256
n0nvme <svmpl3nvm3@gmail.com>
2020-03-07 05:58:38 +0300

    builder helps upgrader when no buld tasks

--- role.builder.js
+++ role.builder.js
@@ -19,6 +19,11 @@ var roleBuilder = {
                     creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
                 }
             }
+            else {
+                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+            }
         } else {
             //var sources = creep.room.find(FIND_SOURCES);
             //if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
