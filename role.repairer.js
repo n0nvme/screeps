23413248b76b92faf62acc595d706e8b49a73f70
n0nvme <svmpl3nvm3@gmail.com>
2020-03-21 22:12:39 +0300

    FUCK SCREEPS AUTODELETE

---
+++ role.repairer.js
@@ -0,0 +1,65 @@
+var roleRepairer = {
+
+    /** @param {Creep} creep **/
+    run: function (creep) {
+
+        if (creep.memory.repairing && creep.carry.energy == 0) {
+            creep.memory.repairing = false;
+            creep.say('🔄 refil');
+        }
+        if (!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.repairing = true;
+            creep.say('🚧 repair');
+        }
+
+        if (creep.memory.repairing) {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: structure => { return (structure.structureType == STRUCTURE_CONTAINER && structure.hits < structure.hitsMax) }
+            });
+            if (targets.length) {
+                if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+            } else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: structure => { return (structure.structureType == STRUCTURE_RAMPART && structure.hits < 80000) }
+                });
+                if (targets.length) {
+                    if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+                    }
+                } else {
+                    var targets = creep.room.find(FIND_STRUCTURES, {
+                        filter: object => object.hits < object.hitsMax && object.hits < 75000
+                    });
+
+                    targets.sort((a, b) => a.hits - b.hits);
+
+                    if (targets.length) {
+                        if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+                        }
+                    }
+                    else {
+                        if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+                        }
+                    }
+                }
+            }
+        } else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER && structure.store.energy > 0);
+                }
+            });
+            if (targets.length > 0) {
+                if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleRepairer;
---
+++ role.scout.js
@@ -0,0 +1,11 @@
+var roleScout = {
+
+    /** @param {Creep} creep **/
+    run: function (creep) {
+        if (creep.claimController(creep.room.controller) != 0) {
+            creep.moveTo(Game.flags.Flag1);
+        }
+    }
+};
+
+module.exports = roleScout;
